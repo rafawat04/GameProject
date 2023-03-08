f65c2ce8a2b1c62e81ed80b56cd9bdb11ee2cd8b
yukari.haneda0421 <yukari.haneda0421@gmail.com>
2023-03-08 10:29:43 +0900

    Enemyにshootingをアタッチ（現段階ではPlayerと同様Mで発射）

--- Assets/Main/Script/HP_Shoot/Shooting.cs
+++ Assets/Main/Script/HP_Shoot/Shooting.cs
@@ -36,11 +36,7 @@ public class Shooting : MonoBehaviour {
             {
                 shotCount -= 1;
 
-            //     GameObject bullets = Instantiate(bullet, this.transform.position, ここにどんなQuaternionを入れる？);
-            // bullets.transform.parent = this.transform;
-
                 GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.parent.eulerAngles.x-90, transform.parent.eulerAngles.y,0));
-                // GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.parent.eulerAngles.x, transform.parent.eulerAngles.y,0));
                 Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
                 bulletRb.AddForce(transform.forward * shotSpeed);
 
